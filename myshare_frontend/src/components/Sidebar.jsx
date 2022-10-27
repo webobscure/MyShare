f60eec5fea3b48572b740c59c9d355a008fc7631
webobscure <sparkygino@gmail.com>
2022-10-27 21:58:54 +0300

    create Sidebar

---
+++ myshare_frontend/src/components/Sidebar.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import { RiHomeFill } from 'react-icons/ri';
+import { IoIosArrowForward } from 'react-icons/io';
+
+import logo from '../assets/logo.png';
+
+const isNotActiveStyle =
+  'flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitilize';
+const isActiveStyle =
+  'flex items-center px-5 gap-3 font-extrabold border-r-2 border-black transition-all duration-200 ease-in-out capitilize';
+
+const categories = [
+  { name: 'Animals' },
+  { name: 'Wallpapers' },
+  { name: 'Photography' },
+  { name: 'Gaming' },
+  { name: 'Coding' },
+  { name: 'Other' },
+];
+
+const Sidebar = ({ user, closeToggle }) => {
+  const handleCloseSidebar = () => {
+    if (closeToggle) closeToggle(false);
+  };
+
+  return (
+    <div className="flex flex-col justify-betwwen bg-white h-full overflow-y-scrikk min-w-210 hide-scrollbar">
+      <div className="flex flex-col">
+        <Link
+          to="/"
+          className="flex px-5 gap-5 my-6 pt-1 w-190 item-center"
+          onClick={handleCloseSidebar}>
+          <img src={logo} alt="logo" className="w-full" />
+        </Link>
+        <div className="flex flex-col gap-5">
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? isActiveStyle : isNotActiveStyle)}
+            onClick={handleCloseSidebar}>
+            <RiHomeFill />
+            Home
+          </NavLink>
+          <h3 className="mt-2 px-5 text-base 2xl:text-xl">
+            {categories.slice(0, categories.length - 1)}.map((category) => (
+            {
+              <NavLink
+                to={`/category/${categories.name}`}
+                className={({ isActive }) => (isActive ? isActiveStyle : isNotActiveStyle)}
+                onClick={handleCloseSidebar}></NavLink>
+            }
+            ))
+          </h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
