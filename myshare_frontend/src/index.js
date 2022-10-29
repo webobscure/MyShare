fd8341e260eb6fb98a770865d7492b8e27792f2d
webobscure <sparkygino@gmail.com>
2022-10-29 14:12:41 +0300

    catch errors

--- myshare_frontend/src/index.js
+++ myshare_frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-
+import {
+    createRoot
+} from 'react-dom/client'
 import {
     BrowserRouter as Router
 } from 'react-router-dom'
@@ -8,9 +9,10 @@ import {
 import './index.css';
 import App from './App'
 
-ReactDOM.render( 
-    <Router>
-        <App />
-    </Router>,
-    document.getElementById('root')
-    );
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
+    <Router >
+        <App tab = "home" />
+    </Router>
+)
