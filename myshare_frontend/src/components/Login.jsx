21dae4b951b3ef149cc54070eec1669babead8b2
webobscure <sparkygino@gmail.com>
2022-10-30 14:47:51 +0300

    check errors

--- myshare_frontend/src/components/Login.jsx
+++ myshare_frontend/src/components/Login.jsx
@@ -10,6 +10,7 @@ import { client } from '../client';
 const Login = () => {
   const navigate = useNavigate();
   const responseGoogle = (response) => {
+    console.log(response);
     localStorage.setItem('user', JSON.stringify(response.profileObj));
     const { name, googleId, imageUrl } = response.profileObj;
     const doc = {
--- myshare_frontend/src/components/Navbar.jsx
+++ myshare_frontend/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { IoMdAdd, IoMdSearch } from 'react-icons/io'
 const Navbar = ({ searchTerm, setSearchTerm, user}) => {
   const navigate = useNavigate();
   
-  // if(!user) return null;
+  if(!user) return null;
 
   return (
     <div className='flex gap-2 md:gap-5 w-full mt-5 pb-7'>
@@ -21,8 +21,11 @@ const Navbar = ({ searchTerm, setSearchTerm, user}) => {
         />
       </div>
       <div className="flex gap-3">
-        <Link to={`user-profile/${user?._id}`}>
+        <Link to={`user-profile/${user?._id}`} className="hidden md:block">
           <img src={user.image} alt="user" className="w-14 h-12 rounded-lg" />
+        </Link>
+        <Link to='create-pin' className="bg-black text-white rounded-lg w-12 h-12 md:w-14 md:h-12 flex justify-center items-center">
+          <IoMdAdd />
         </Link>
       </div>
     </div>
