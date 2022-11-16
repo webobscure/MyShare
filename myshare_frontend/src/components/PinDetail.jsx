9f0f0ecdad4c6993a47ba36b1daa64501650f459
webobscure <sparkygino@gmail.com>
2022-11-16 21:52:06 +0300

    add comments section

--- myshare_frontend/src/components/PinDetail.jsx
+++ myshare_frontend/src/components/PinDetail.jsx
@@ -16,6 +16,30 @@ const PinDetails = ({ user }) => {
   const [addingComment, setAddingComment] = useState(false);
   const { pinId } = useParams();
 
+  const addComment = () => {
+    if(comment) {
+      setAddingComment(true);
+
+      client
+      .patch(pinId)
+      .setIfMissing({comments: []})
+      .insert('after', 'comments[-1]', [{
+        comment,
+        _key: uuidv4(),
+        postedBy: {
+          _type: 'postedBy',
+          _ref: user._id
+        }
+       }])
+       .commit()
+       .then(() => {
+        fetchPinDetails();
+        setComment('');
+        setAddingComment(false);
+       })
+    }
+  }
+
 
   const fetchPinDetails = () => {
     let query = pinDetailQuery(pinId);
@@ -91,8 +115,37 @@ const PinDetails = ({ user }) => {
               alt="user-profile"
               className="w-10 h-10 rounded-full cursor-pointer"
             />
+            <div className="flex flex-col">
+              <p className="font-bold">
+                {comment.postedBy.userName}
+              </p>
+              <p>{comment.comment}</p>
+            </div>
           </div>
         ))}
+      </div>
+      <div className="flex flex-wrap mt-6 gap-3">
+      <Link to={`/user-profile/${pinDetail.postedBy?._id}`} >
+        <img
+          className="w-10 h-10 rounded-full cursor-pointer "
+          src={pinDetail.postedBy?.image}
+          alt="user-profile"
+        />
+      </Link>
+      <input 
+      type="text" 
+      className="flex-1 border-gray-100 outline-none border-2 p-2 rounded-2xl focus:border-gray-300"
+      placeholder='Add a comment'
+      value={comment}
+      onChange={(e) => setComment(e.target.value)}
+      />
+      <button
+      type="button"
+      className="bg-red-500  text-white rounded-full px-6 py-2 font-semibold text-base outline-none"
+      onClick={addComment}
+      >
+        {addingComment ? 'Posting the commment...' : 'Post'}
+      </button>
       </div>
     </div>
   )
